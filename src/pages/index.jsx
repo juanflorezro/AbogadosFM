587928c3327f9acaa4a7969080b0709955edae46
juanflorezro <juan10florez10@gmail.com>
2024-08-13 19:21:15 -0500

    funcionalidad al boton descargar de la tabla

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -254,7 +254,7 @@ export const Home = () => {
                                                             <path d="M10 12a2 2 0 1 0 4 0a2 2 0 0 0 -4 0" />
                                                             <path d="M21 12c-2.4 4 -5.4 6 -9 6c-3.6 0 -6.6 -2 -9 -6c2.4 -4 5.4 -6 9 -6c3.6 0 6.6 2 9 6" />
                                                         </svg></button>
-                                                        <button className='btn-g'><svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-download" width="20" height="20" viewBox="0 0 24 24" strokeWidth="1.5" stroke="#00bfd8" fill="none" strokeLinecap="round" strokeLinejoin="round">
+                                                        <button className='btn-g' onClick={()=> exportToExcel([caso])}><svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-download" width="20" height="20" viewBox="0 0 24 24" strokeWidth="1.5" stroke="#00bfd8" fill="none" strokeLinecap="round" strokeLinejoin="round">
                                                             <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                                                             <path d="M4 17v2a2 2 0 0 0 2 2h12a2 2 0 0 0 2 -2v-2" />
                                                             <path d="M7 11l5 5l5 -5" />
