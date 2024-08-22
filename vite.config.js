4159232c4d718119ee10dd00b4d6f180bce8415f
juanflorezro <juan10florez10@gmail.com>
2024-08-21 23:21:32 -0500

    veuiuite .cof . s

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{
-    port: 3000
-  },
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom'], // Ejemplo de chunk manual
-        },
-      },
-    },
-    chunkSizeWarningLimit: 1500, // Ajuste del límite
-  },
 })
